e994395e3c6b6329daabab2a7c3d8fb19faf10b9
Suiter <justinsuiter4@gmail.com>
2024-10-09 08:26:55 -0400

    Added to color to our Print Functions

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,8 +1,25 @@
 import sys
 import time
 
-# Display a welcome message when the program starts
-print("\nWelcome to InfoTechCenter V1.0\n")
+# Define ANSI escape codes for various colors and reset
+RESET = "\033[0m"
+BLACK = "\033[30m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+WHITE = "\033[37m"
+
+# You can change the color below to any defined color (e.g., RED, GREEN, BLUE, etc.)
+welcome_color = CYAN
+selected_color = BLUE  # Change this to any color you prefer
+exit_color = GREEN
+
+
+# Display a welcome message when the program starts (using the selected color)
+print(welcome_color + "\nWelcome to InfoTechCenter V1.0\n" + RESET)
 
 # Initialize variables for loop control
 x = 0  # Counter to track the number of iterations
@@ -12,8 +29,8 @@ ellipsis = 0  # Counter for adding dots after the booting message
 while x != 20:
     x += 1  # Increment the counter on each iteration
 
-    # Construct a message that changes by adding one more dot (.)
-    message = ("InfoTechCenter System Booting" + "." * ellipsis)
+    # Construct a message that changes by adding one more dot (in the selected color)
+    message = selected_color + "InfoTechCenter System Booting" + "." * ellipsis + RESET
 
     # Increment the number of dots after the message
     ellipsis += 1
@@ -28,6 +45,6 @@ while x != 20:
     if ellipsis == 4:
         ellipsis = 0
 
-    # When the loop finishes after 20 iterations, display a final boot-up message
+    # When the loop finishes after 20 iterations, display a final boot-up message (in the selected color)
     if x == 20:
-        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
+        print(exit_color + "\n\nOperating System Booted Up - Retina Scanned - Access Granted" + RESET)
