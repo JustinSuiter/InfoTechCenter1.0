758e510e94387eb00dc753ff070a2dc1ed1833f5
Suiter <justinsuiter4@gmail.com>
2024-10-15 08:08:40 -0400

    Added more Elif and an Else statement to the VRS function - Stable

--- CastleBravo.py
+++ CastleBravo.py
@@ -24,5 +24,23 @@ def vehicleResponseSystem():
               "of the forecast of", weatherAlert, "weather conditions.")
         sleep(1)
         print("\nVRS System has been engaged only allowing you to drive 45mph")
-
+    elif weatherAlert == "rainy":
+        print("\nThe Nation Weather Service has updated our alarm by 15 minutes because "
+              "of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS System has been engaged only allowing you to drive 65mph")
+    elif weatherAlert == "windy":
+        print("\nThe Nation Weather Service has updated our alarm by 5 minutes because "
+              "of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS System has been engaged only allowing you to drive 70mph")
+    elif weatherAlert == "icy":
+        print("\nThe Nation Weather Service has updated our alarm by 50 minutes because "
+              "of the forecast of", weatherAlert, "weather conditions.")
+        sleep(1)
+        print("\nVRS System has been engaged only allowing you to drive 35mph")
+    else:
+        print("The NWS is calling for", weatherAlert, "skies, drive carefully to get to your destination!")
+        sleep(1)
+        print("\nVRS System has been disengaged")
 vehicleResponseSystem()
